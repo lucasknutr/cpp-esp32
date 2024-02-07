84f4baa35110c0fb60b4cfa980e8abb7eb63781f
Lucas <lucasclaudinocc@gmail.com>
2024-02-07 16:30:18 -0300

    Testing relays and button functionality

--- Blink.ino
+++ Blink.ino
@@ -1,26 +1,40 @@
 int LED_BUILTIN = 2;
-int BUTTON_PIN = 5;
+int SONDA = 23;
 int RELE1 = 21;
 int RELE2 = 19;
-int SONDA = 23;
+int TERMOMETRO = 12;
+int BUTTON_PIN = 5;
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(SONDA, INPUT_PULLUP);
+  pinMode(RELE1, OUTPUT);
+  pinMode(RELE2, OUTPUT);
+  pinMode(TERMOMETRO, INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  if (digitalRead(BUTTON_PIN) == LOW) {
+  // Envia sinal para o receptor (no caso, a Jetson), será utilizado para desligamento do aparelho 
+  // Verifica se a sonda está conectada
+  while(digitalRead(SONDA) == HIGH) {
+    if(digitalRead(BUTTON_PIN) == LOW){
+      digitalWrite(RELE2, HIGH);
+      delay(1000);
+      digitalWrite(RELE1, HIGH);
+    }
+    else if (digitalRead(LED_BUILTIN) == HIGH) {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+  }
+
+  if(digitalRead(BUTTON_PIN) == LOW){
+    digitalWrite(RELE2, LOW);
     delay(1000);
-    Serial.println("SIGNAL");
+    digitalWrite(RELE1, LOW);
+  }
+  else if (digitalRead(LED_BUILTIN) == LOW) {
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-  } else if (digitalRead(SONDA) == LOW) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("SINAL TA MASSA");
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
   }
 }
